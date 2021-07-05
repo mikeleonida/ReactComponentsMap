81dc08fd7dc83310f6db4f6c02175bd431d809eb
mikeleonida <74788426+mikeleonida@users.noreply.github.com>
2021-07-05 12:32:03 -0400

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Detail from "./Detail";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <p>{props.id}</p>
+        <h2 className="name">{props.name}</h2>
+        <img className="circle-img" src={props.img} alt="avatar_img" />
+      </div>
+      <div className="bottom">
+        <Detail detailInfo={props.phone} />
+        <Detail detailInfo={props.email} />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/contacts.js
@@ -0,0 +1,34 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Taylor Swift",
+    imgURL: "",
+    phone: "+123 456 789",
+    email: "taylor@amazon.com"
+  },
+  {
+    id: 2,
+    name: "Jack Bauer",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    id: 3,
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  },
+  {
+    id: 4,
+    name: "Michael",
+    imgURL: "",
+    phone: "+123 456 789",
+    email: "mike@hotmail.com"
+  }
+];
+
+export default contacts;
